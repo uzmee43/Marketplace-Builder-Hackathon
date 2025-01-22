852d6419f537f02483fddbd8f5ade966eba2b1c8
uzmee43 <uk0172634@gmail.com>
2025-01-22 17:53:26 +0500

    first commit

---
+++ app/(client)/page.tsx
@@ -0,0 +1,32 @@
+import Container from "@/components/Container";
+import HomeBanner from "@/components/HomeBanner";
+import ProductGrid from "@/components/ProductGrid";
+import Sofa from "@/components/Sofa";
+import SofaSet from "@/components/SofaSet";
+import Our_blog from "@/components/About";
+import InstagramSection from "@/components/Insta";
+
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <Sofa/>
+      </div>
+     
+      <Container className="py-10">
+        <HomeBanner />
+        <ProductGrid />
+      </Container>
+      <div>
+        <SofaSet/>
+      </div>
+      <div>
+        <Our_blog/>
+      </div>
+      <div>
+        <InstagramSection/>
+      </div>
+      
+    </div>
+  );
+}
